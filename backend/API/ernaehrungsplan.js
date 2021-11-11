38d0dfa7d1230f8d131f44f65420150bd2842a31
Sgt_Epic <adrian.koss@fh-bielefeld.de>
2021-11-11 18:17:35 +0100

    adds a skeleton for the ernaehrungsplan api, User Story: 33 Backend)

---
+++ backend/API/ernaehrungsplan.js
@@ -0,0 +1,33 @@
+/*****************************************
+ * Module imports for all required modules
+ ****************************************/
+const express = require('express')
+/**
+ * The router allows us to receive requests in files that aren't the main file
+ */
+const router = express.Router()
+
+/*******************************************************************************
+ * Middleware for the server to use
+ * Middleware has to be specified for every router, it isn't enough to just tell
+ * the main express app what middleware to use
+ ******************************************************************************/
+router.use(express.urlencoded({ extended: false }))
+router.use(express.json())
+
+/*******************************************************************************
+ * The request implementation
+ ******************************************************************************/
+router.post('/', (req, res) => {
+
+})
+
+router.put('/', (req, res) => {
+
+})
+
+router.get('/', (req, res) => {
+
+})
+
+module.exports = router
