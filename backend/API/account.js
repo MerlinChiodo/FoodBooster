2d7710cf8ed7fbd4b818778656343c350206e036
Sgt_Epic <adrian.koss@fh-bielefeld.de>
2021-11-11 18:11:57 +0100

    adds a skeleton for the account api, User Story: 33 Backend

---
+++ backend/API/account.js
@@ -0,0 +1,47 @@
+/*****************************************
+ * Module imports for all required modules
+ ****************************************/
+const express = require('express')
+/**
+ * The router allows us to receive requests in files that aren't the main file
+ */
+const router = express.Router()
+
+/*******************************************************************************
+ * Middleware for the server to use
+ * Middleware has to be specified for every router, it isn't enough to just tell
+ * the main express app what middleware to use
+ ******************************************************************************/
+router.use(express.urlencoded({ extended: false }))
+router.use(express.json())
+
+/*******************************************************************************
+ * The request implementation
+ ******************************************************************************/
+router.post('/', (req, res) => {
+
+})
+
+router.delete('/', (req, res) => {
+
+})
+
+router.put('/', (req, res) => {
+
+})
+
+router.put('/password', (req, res) => {
+
+})
+
+router.post('/favorite', (req, res) => {
+
+})
+
+router.get('/favorite', (req, res) => {
+
+})
+
+router.get('/rezept', (req, res) => {
+
+})
