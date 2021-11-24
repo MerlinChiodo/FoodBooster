9bed451e76a8f8853f58c4ae2baf1751b4626074
jgeist1 <janik.geist@fh-bielefeld.de>
2021-11-24 12:50:05 +0100

    Pack-Lock.json

---
+++ backend/controller/rezept.js
@@ -0,0 +1,17 @@
+
+const getRecipes = async (req,res) => {
+
+    //change recipes to get field TAGS
+
+    //No Filtering needed
+    if(!req.query){
+
+    }
+
+    //If query exists, filtering is needed
+    const {kategorie,name,zutaten,erstelleraccount,tags} = req.query
+
+
+
+
+}
