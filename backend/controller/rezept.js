3a8f163471c635d27c8fafab05d41d8040547bf7
SgtEpic <87912888+SgtEpic@users.noreply.github.com>
2021-12-02 12:01:45 +0100

    adds the featured recipe API and tests (#13)

--- backend/controller/rezept.js
+++ backend/controller/rezept.js
@@ -2,8 +2,7 @@
  * Module imports for all required modules
  ****************************************/
 const prisma = require(`../prismaClient.js`)
-const underscore = require('underscore');
-
+const underscore = require('underscore')
 
 /**
  *
@@ -28,52 +27,95 @@ const underscore = require('underscore');
  *
  *  TODO : Database cleanup --> Attributes are mixed with english and german
  * */
-const getRecipes = async (req,res) =>{
+const getRecipes = async (req, res) => {
 
-        //No special query defined --> return full list of recepies
-        if ( underscore.isEmpty( req.query ) ){
+  //No special query defined --> return full list of recepies
+  if (underscore.isEmpty(req.query)) {
 
-            const recipes = await prisma.recipe.findMany()
+    const recipes = await prisma.recipe.findMany()
 
-            return res.status(200).json( { success: true, msg: recipes } )
-        }
+    return res.status(200).json({ success: true, msg: recipes })
+  }
 
-        try {
-            //If sorting is requested, we can use prisma built-ins
-            const recipesFiltered = await prisma.recipe.findMany({
-                where: {
-                    //Filters using AND --> name & servings, only returns recepies with given name AND servings
-                    AND: [
-                        {name: req.query.name},
-                        //Using ternary operator, to cast without error
-                        { bewertung: ( !req.query.bewertung ) ? undefined : Number( req.query.bewertung ) },
-                        { creatorID: ( !req.query.author ) ? undefined : Number( req.query.author ) },
-                        { servings: ( !req.query.servings ) ? undefined : Number( req.query.servings ) },
-                        { featured: ( !req.query.featured ) ? undefined : ( req.query.featured === "true" ) },
-                        { description: {
-                            contains: req.query.description }
-                        },
-                        { ingredients: {
-                                every: {
-                                    ingredientName: { in: req.query.ingredients }
-                                },
-                            },
-                        },
-                        { category: {
-                            every: {
-                                categoryName: { in: req.query.categories }
-                            },
-                            },
-                        }
-                    ],
-                }
-            } )
+  try {
+    //If sorting is requested, we can use prisma built-ins
+    const recipesFiltered = await prisma.recipe.findMany({
+      where: {
+        //Filters using AND --> name & servings, only returns recepies with given name AND servings
+        AND: [
+          { name: req.query.name },
+          //Using ternary operator, to cast without error
+          {
+            bewertung: (!req.query.bewertung) ? undefined : Number(
+              req.query.bewertung),
+          },
+          {
+            creatorID: (!req.query.author) ? undefined : Number(
+              req.query.author),
+          },
+          {
+            servings: (!req.query.servings) ? undefined : Number(
+              req.query.servings),
+          },
+          {
+            featured: (!req.query.featured)
+              ? undefined
+              : (req.query.featured === 'true'),
+          },
+          {
+            description: {
+              contains: req.query.description,
+            },
+          },
+          {
+            ingredients: {
+              every: {
+                ingredientName: { in: req.query.ingredients },
+              },
+            },
+          },
+          {
+            category: {
+              every: {
+                categoryName: { in: req.query.categories },
+              },
+            },
+          },
+        ],
+      },
+    })
 
-            return res.status(200).json( { success: true, msg: recipesFiltered} )
-        } catch(err){
-            console.log(err)
-            return res.status(500).json( { success: false, err: "Ups, something went wrong!" } )
-        }
+    return res.status(200).json({ success: true, msg: recipesFiltered })
+  } catch (err) {
+    return res.status(500).
+      json({ success: false, err: 'Ups, something went wrong!' })
+  }
 }
 
-module.exports = { getRecipes }
+/**
+ * Function to get all featured recipes
+ * Looks for all recipes where the featured attribute is set to true
+ *
+ * Returns status code 200 and a list of all featured recipes ordered by rating
+ * If there is a database error the response is a status code 500 and the error
+ */
+const getFeatured = async (req, res) => {
+  let featured = []
+  try {
+    featured = await prisma.recipe.findMany({
+      where: {
+        featured: true,
+      },
+      orderBy: {
+        rating: 'desc',
+      },
+    })
+    return res.status(200).send({ success: true, featured })
+  } catch (error) {
+    return res.status(500).
+      json({ success: false, err: 'Ups, something went wrong!', error })
+  }
+
+}
+
+module.exports = { getRecipes, getFeatured }
