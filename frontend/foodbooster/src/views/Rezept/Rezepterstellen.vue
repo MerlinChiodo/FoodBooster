2e61ccdddc8453317c2148f2d7cb5a1ff3f8b207
Paul Merlin Nagel <p-merlin@web.de>
2021-12-16 22:05:43 +0100

    commenting

--- frontend/foodbooster/src/views/Rezept/Rezepterstellen.vue
+++ frontend/foodbooster/src/views/Rezept/Rezepterstellen.vue
@@ -41,6 +41,22 @@
               >
               </ui-slider>
 
+              <!--              <ui-list role="group">-->
+              <!--                <template v-for="(item, index) in items">-->
+              <!--                  <ui-item-divider v-if="item === '-'" :key="index"></ui-item-divider>-->
+              <!--                  <ui-item v-else :key="index">-->
+              <!--                    <ui-item-text-content>{{ item.text }}</ui-item-text-content>-->
+              <!--                    <ui-item-last-content>-->
+              <!--                      <ui-checkbox-->
+              <!--                          v-model="checkedValues"-->
+              <!--                          :value="item.value"-->
+              <!--                          @click.stop-->
+              <!--                      ></ui-checkbox>-->
+              <!--                    </ui-item-last-content>-->
+              <!--                  </ui-item>-->
+              <!--                </template>-->
+              <!--              </ui-list>-->
+
 
               <!-- SUBMIT -->
               <ui-form-field v-if="name" :class="actionClass">
@@ -71,6 +87,9 @@ const FormData = require('form-data');
 
 export default {
   name: "Rezept_erstellen",
+  mounted() {
+
+  },
   data() {
     return {
 
