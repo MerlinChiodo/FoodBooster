9203f9dacf63dae6234ee03783896ae679461704
“[Anna <[anna_sabrina.heim@fh-bielefeld.de]>
2021-12-18 16:23:56 +0100

    sytle korrektur, bewertungssystem ueberarbeitet, funktionalitaet von bewertungen und kommentaren schreiben, noch nicht fehlerfrei

--- frontend/foodbooster/src/views/Rezept/rezeptDummy.vue
+++ frontend/foodbooster/src/views/Rezept/rezeptDummy.vue
@@ -3,7 +3,7 @@
   <ui-grid class="head-grid">
     <ui-grid-cell class="demo-cell" columns="12">
       <ui-form-field>
-        <h1>{{name}}</h1>
+        <h1>{{ name }}</h1>
       </ui-form-field>
     </ui-grid-cell>
   </ui-grid>
@@ -37,23 +37,51 @@
   </ui-grid>
 
 
-  <ui-grid class="bewertungUndNutriUndAnleitung">
+  <ui-grid class="bewertungUndNutri">
     <ui-grid-cell class="bewertung" columns="3">
-      <ui-icon-button v-model="value1" :toggle="icon1"></ui-icon-button>
-      <ui-icon-button v-model="value2" :toggle="icon1"></ui-icon-button>
-      <ui-icon-button v-model="value3" :toggle="icon1"></ui-icon-button>
-      <ui-icon-button v-model="value4" :toggle="icon1"></ui-icon-button>
-      <ui-icon-button v-model="value5" :toggle="icon1"></ui-icon-button>
-      <p>{{totalRatings}} Bewertungen</p>
+      <div class="bild">
+        <img src="../../assets/sterne.jpg" alt="Bild konnte nicht geladen werden."><!--Variable ersetzen-->
+      </div>
+      <ui-form-field>
+        <ui-radio :disabled="schalter" v-model="bewertung" @click="ausgrauen" input-id="eins"
+                  value="1" v-on:change="aufrufen"></ui-radio>
+        <label>1</label>
+      </ui-form-field>
+      <ui-form-field>
+        <ui-radio :disabled="schalter" v-model="bewertung" @click="ausgrauen" input-id="zwei"
+                  value="2" v-on:change="aufrufen"></ui-radio>
+        <label>2</label>
+      </ui-form-field>
+      <ui-form-field>
+        <ui-radio :disabled="schalter" v-model="bewertung" @click="ausgrauen" input-id="drei"
+                  value="3" v-on:change="aufrufen"></ui-radio>
+        <label>3</label>
+      </ui-form-field>
+      <ui-form-field>
+        <ui-radio :disabled="schalter" v-model="bewertung" @click="ausgrauen" input-id="vier"
+                  value="4" v-on:change="aufrufen"></ui-radio>
+        <label>4</label>
+      </ui-form-field>
+      <ui-form-field>
+        <ui-radio :disabled="schalter" v-model="bewertung" @click="ausgrauen" input-id="fuenf"
+                  value="5" v-on:change="aufrufen"></ui-radio>
+        <label>5</label>
+      </ui-form-field>
+
+      <p>{{ totalRatings }} Bewertungen</p>
     </ui-grid-cell>
 
-    <ui-grid-cell class="nutri" columns="2">
+    <ui-grid-cell class="nutri" columns="3">
       <img src="../../assets/nutri.jpg" alt="Bild konnte nicht geladen werden."><!--Variable ersetzen-->
     </ui-grid-cell>
 
+    <ui-grid-cell class="leer" columns="6">
+    </ui-grid-cell>
+  </ui-grid>
 
 
-    <ui-grid-cell class="anleitung" columns="5">
+  <ui-grid class="description">
+    <ui-grid-cell class="anleitung" columns="6">
       <ui-card outlined class="demo-card">
         <div>
           Beschreibung:
@@ -74,23 +102,31 @@
       </ui-card>
     </ui-grid-cell>
 
-
-
-
-    <ui-grid-cell class="leer" columns="2">
+    <ui-grid-cell class="leer" columns="6">
     </ui-grid-cell>
   </ui-grid>
 
 
   <ui-grid class="kommentare">
-    <ui-grid-cell class="schreiben" columns="6">
-      <ui-textfield input-type="textarea" rows="20" cols="120">
+    <ui-grid-cell class="schreiben" columns="3">
+      <ui-textfield v-model="kommentar" input-type="textarea" rows="20" cols="120">
         Schreiben Sie einen Kommentar.
       </ui-textfield>
+      <ui-card-actions full-bleed>
+        <ui-button class="demo-card-action" @click="kommentarSchreiben">
+          Kommentieren
+          <template #after>
+            <ui-icon>arrow_forward</ui-icon>
+          </template>
+        </ui-button>
+      </ui-card-actions>
     </ui-grid-cell>
 
-    <ui-grid-cell class="lesen" columns="6">
+    <ui-grid-cell class="lesen" columns="9">
       Andere Kommentare <!--einlesen-->
+    </ui-grid-cell>
+
+    <ui-grid-cell class="leer" columns="0">
     </ui-grid-cell>
   </ui-grid>
 
@@ -112,9 +148,13 @@ export default {
       description: null,
       featured: null,
       rating: null,
-      totalRatings: null,
+      totalRatings: 0,
       recipeID: 7,
       recipe: null,
+      bewertung: null,
+      kommentar: "",
+      schalter: false,
+
 
       value0: 1,
       value1: false,
@@ -128,18 +168,67 @@ export default {
       },
     };
   },
+  methods: {
+    aufrufen() {
+      this.userBewertung();
+      this.bewertungszaehler();
+    },
+
+
+    bewertungszaehler() {
+      this.totalRatings++
+    },
+    userBewertung() {
+      this.rating = this.bewertung.value;
+    },
+    ausgrauen() {
+      this.schalter = true;
+    }
+  },
 
   async mounted() {    // GET request using axios with async/await
-    const response = await http.get("rezept/single/"+this.recipeID, {
-        }
+    const response = await http.get("rezept/single/" + this.recipeID, {}
     );
-    this.name=response.data.msg.name;
-    this.servings=response.data.msg.servings;
-    this.description=response.data.msg.description;
-    this.rating=response.data.msg.rating;
-    this.featured=response.data.msg.featured;
-    this.totalRatings=response.data.msg.totalRatings;
+    this.name = response.data.msg.name;
+    this.servings = response.data.msg.servings;
+    this.description = response.data.msg.description;
+    this.rating = response.data.msg.rating;
+    this.featured = response.data.msg.featured;
+    this.totalRatings = response.data.msg.totalRatings;
+  },
+  fortmatResponse(res) {
+    return JSON.stringify(res, null, 2);
+  },
 
+  async postKommentar() {
+    try {
+      const res = await http.post("/api/rezept/kommentar/", {
+                recipeID: this.recipeID,
+                kommentarID: 0, //ID sollte aus dem Backend kommen
+                kommentar: this.kommentar,
+
+              }
+              ,
+              {
+                headers: {
+                  "x-access-token":
+                      "token-value",
+                }
+                ,
+              }
+          )
+      ;
+
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: res.headers,
+        data: res.data,
+      };
+
+      this.postSuccessResult = this.fortmatResponse(result);
+    } catch (err) {
+      this.postResult = this.fortmatResponse(err.response?.data) || err;
+    }
   },
 
 
@@ -151,8 +240,8 @@ export default {
 
 <style scoped>
 
-.demo-card-article{
-  color:#0000008A;
+.demo-card-article {
+  color: #0000008A;
   padding: 15px;
 }
 
